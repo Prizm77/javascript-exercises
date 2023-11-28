8f1378c50a21dcbc1a4102d5a64835a3583da713
EricZ <ericzaitsev@gmail.com>
2023-11-27 21:02:30 -0800

    completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookAry) {
+    let titles = [];
+
+    bookAry.forEach(book => {
+        titles.push(book.title);
+    }
+    )
+    return titles;
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = null;
+
+    people.forEach(person => {
+        let currentAge = getAge(person.yearOfBirth, person.yearOfDeath)
+
+        if (currentAge > oldestAge) {
+            oldestAge = currentAge;
+            oldestPerson = person;
+        }
+    }
+    )
+    return oldestPerson;
 
 };
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+      }
+      return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
